ed3f494de75352cbcf0885c037432d3ac233f4d7
hajin428 <amm_321@naver.com>
2024-10-13 23:40:51 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 1.58 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,25 +1,29 @@
 class Solution {
     public int[] solution(int[] arr) {
+        // 길이가 1이면 -1을 리턴
+        if (arr.length == 1) {
+            return new int[]{-1};
+        }
+
+        // 최소값을 찾기 위한 인덱스
+        int minIndex = 0;
         
-        if(arr.length == 1){
-          int[] answer = {-1};
-          return answer;
-      }
+        // 배열을 순회하면서 최소값의 인덱스를 찾음
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[minIndex] > arr[i]) {
+                minIndex = i;
+            }
+        }
+        
+        // 새로운 배열에 최소값을 제외한 값을 복사
+        int[] answer = new int[arr.length - 1];
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i != minIndex) {
+                answer[index++] = arr[i];
+            }
+        }
 
-      int[] answer = new int[arr.length-1];
-      int minIndex=0;
-
-      for(int i=0;i<arr.length;i++){
-          if(arr[minIndex]>arr[i]){
-              minIndex = i;
-          }
-      }
-      for(int i=minIndex+1;i<arr.length;i++){
-          arr[i-1] = arr[i];
-      }
-      for(int i=0;i<answer.length;i++){
-          answer[i] = arr[i];
-      }
-      return answer;
+        return answer;
     }
 }
