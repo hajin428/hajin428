a15a9c4b54a73b074d65ee53a11b11a5bfb36192
hajin428 <amm_321@naver.com>
2024-12-05 22:26:14 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        //같으면 1
+        //다르면 -1
+        
+        if (num1 == num2){
+            int answer = 1;
+            return answer;
+        } else {
+            int answer = -1;
+            return answer;
+        }
+    }
+}
