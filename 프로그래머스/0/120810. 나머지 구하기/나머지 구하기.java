692fd9260e92ad0d9afe19454679046967df0878
hajin428 <amm_321@naver.com>
2024-12-09 03:40:12 +0900

    [level 0] Title: 나머지 구하기, Time: 0.04 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int a = num1;
+        int b = num2;
+        int c;
+        
+        c = a%b;
+        return c;
+    }
+}
